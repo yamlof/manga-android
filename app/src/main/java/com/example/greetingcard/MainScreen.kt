afd1f84e1830a7744f31c34a1915925b99560f3e
Edwin Igbinoba <jamesedwin204@gmail.com>
2024-11-24 22:11:30 +0000

    first android app

---
+++ app/src/main/java/com/example/greetingcard/MainScreen.kt
@@ -0,0 +1,77 @@
+package com.example.greetingcard
+
+import android.app.Notification
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.greetingcard.pages.HomePage
+import com.example.greetingcard.pages.NotificationPage
+import com.example.greetingcard.pages.SettingsPage
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+
+    val navItemList = listOf(
+        NavItem("Home", Icons.Default.Home,0),
+        NavItem("Notification", Icons.Default.Notifications,5),
+        NavItem("Settings", Icons.Default.Settings,0),
+    )
+
+    var selectedIndex by remember {
+        mutableIntStateOf(0)
+    }
+
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                navItemList.forEachIndexed {index, navItem ->
+                    NavigationBarItem(
+                        selected = selectedIndex == index,
+                        onClick = {
+                            selectedIndex = index
+                        },
+                        icon = {
+                                BadgedBox(badge =  {
+                                    if(navItem.badgeCount>0)
+                                        Badge(){
+                                            Text(text = navItem.badgeCount.toString() )
+                                        }
+                                    }) {
+                                    Icon(imageVector = navItem.icon, contentDescription = "Icon")
+                                }
+                        },
+                        label = {
+                            Text(text = navItem.label)
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        ContentScreen(modifier = Modifier.padding(innerPadding),selectedIndex)
+
+    }
+    
+}
+
+@Composable
+fun ContentScreen(modifier: Modifier = Modifier,selectedIndex:Int) {
+    when(selectedIndex){
+        0 -> HomePage()
+        1 -> NotificationPage()
+        2 -> SettingsPage()
+    }
+}
---
+++ app/src/main/java/com/example/greetingcard/NavItem.kt
@@ -0,0 +1,9 @@
+package com.example.greetingcard
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavItem(
+    val label : String,
+    val icon : ImageVector,
+    val badgeCount : Int,
+)
