3a5ef378e0bb12a3b38dc17ce31c841bf836bba7
Edwin Igbinoba <jamesedwin204@gmail.com>
2024-11-25 13:35:52 +0000

    first step for cards items

--- app/src/main/java/com/example/greetingcard/pages/HomePage.kt
+++ app/src/main/java/com/example/greetingcard/pages/HomePage.kt
@@ -1,17 +1,35 @@
 package com.example.greetingcard.pages
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.greetingcard.R
+import java.time.format.TextStyle
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun HomePage(modifier: Modifier = Modifier) {
     Column(
@@ -26,5 +44,66 @@ fun HomePage(modifier: Modifier = Modifier) {
             fontWeight = FontWeight.SemiBold,
             color = Color.White
         )
+
+        val painter = painterResource(id = R.drawable.shadow)
+        val description = "Sunny"
+        val title = "Shadow Slave"
+        Box(modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(16.dp)
+        ){
+            ImageCard(
+                painter = painter,
+                contentDescription = description,
+                title = title
+            )
+        }
+
+    }
+
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title:String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+    ){
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
+                )
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(title,style = androidx.compose.ui.text.TextStyle(
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
+                )
+            }
+        }
     }
 }
